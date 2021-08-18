55df880c161c51cf40c79840d8a5b3fc0847b018
gloriachen <chen.gloria@outlook.com>
2021-08-18 16:42:55 +1000

    Added Instructors and Admin fixtures

---
+++ src/DataFixtures/AdminFixture.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AdminFixture extends BaseFixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    // Load Admin Data
+    public function loadData(ObjectManager $manager)
+    {
+        $this->createMany(5, 'admin', function($i) {
+            $admin = new User();
+            $admin->setEmail(sprintf('admin%d@lesmills.com.au', $i));
+            $admin->setUsername($this->faker->userName);
+            $admin->setRoles(['ROLE_ADMIN']);
+            $admin->setPassword($this->passwordHasher->hashPassword(
+                $admin,
+                'lesmills'
+            ));
+
+            return $admin;
+        });
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/InstructorFixture.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class InstructorFixture extends BaseFixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    // Load Instructors Data
+    public function loadData(ObjectManager $manager)
+    {
+        $this->createMany(10, 'instructors', function($i) {
+            $instructor = new User();
+            $instructor->setEmail(sprintf('instructor%d@lesmills.com.au', $i));
+            $instructor->setUsername($this->faker->userName);
+            $instructor->setPassword($this->passwordHasher->hashPassword(
+                $instructor,
+                'lesmills'
+            ));
+
+            return $instructor;
+        });
+
+        $manager->flush();
+    }
+}
