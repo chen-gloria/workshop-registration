a240e4c9cdc2fb1c5b07d3a1228b286450be8218
gloriachen <chen.gloria@outlook.com>
2021-08-22 21:51:40 +1000

    Updated account twig, wait for clarify out!

--- src/Controller/WorkshopController.php
+++ src/Controller/WorkshopController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\WorkshopRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 class WorkshopController extends AbstractController
 {
     /**
