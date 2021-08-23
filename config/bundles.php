fbc689fc145ff6442fa02a82eeb921eba824cc86
gloriachen <chen.gloria@outlook.com>
2021-08-23 19:31:56 +1000

    Fixed templates, deleted translations, and trying to add forms

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
--- src/Controller/WorkshopAdminController.php
+++ src/Controller/WorkshopAdminController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Workshop;
+use App\Form\WorkshopFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class WorkshopAdminController extends AbstractController
 {
     /**
      * @Route("/admin/workshop/create", name="admin_workshop_create")
+     * @IsGranted("ROLE_ADMIN")
      */
-    public function create(): Response
+    public function create(EntityManagerInterface $entityManager, Request $request): Response
     {
-        return $this->render('workshop_admin/create.html.twig');
+        $form = $this->createForm(WorkshopFormType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            /** @var Workshop $workshop */
+            $workshop = $form->getData();
+            $entityManager->persist($workshop);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'This workshop has been created!');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('workshop_admin/create.html.twig', [
+            'workshopForm' => $form->createView(),
+        ]);
     }
 
     /**
